0ff2a71f152745469216f2f2873079f4d23f8d3f
qingyuan.cao@opay-inc.com <qingyuan.cao@opay-inc.com>
2023-08-01 14:09:48 +0800

    修改提交

--- src/main/java/com/rock/hadoop/core/hdfs/service/HdfsService.java
+++ src/main/java/com/rock/hadoop/core/hdfs/service/HdfsService.java
@@ -9,11 +9,30 @@ import java.io.RandomAccessFile;
  * 进行hdfs的基本操作
  */
 public class HdfsService {
-    public static void main(String[] args) throws IOException {
-        String hdfsUrl="hdfs://127.0.0.1:19000";
+    public static void main(String[] args) throws Exception {
+//        write();
+        read();
+    }
+
+    public static void read() throws Exception {
+        String hdfsUrl="hdfs://127.0.0.1:9000";
         HdfsUtil hdfsUtil=new HdfsUtil(hdfsUrl);
-        RandomAccessFile randomAccessFile=new RandomAccessFile("conf/int3.txt","rw");
-//        StringBuilder sb=new StringBuilder();
+        //输出到控制台-OK
+        hdfsUtil.readFile("/input/local_int4.txt");
+
+        //ok
+//        hdfsUtil.downloadFileByte("local_int3.txt");
+
+//        String[] fileList = hdfsUtil.getFileList("local_int3.txt");
+//        for(String url:fileList){
+//            System.out.println(url);
+//        }
+    }
+
+    public static void write() throws IOException {
+        String hdfsUrl="hdfs://127.0.0.1:9000";
+        HdfsUtil hdfsUtil=new HdfsUtil(hdfsUrl);
+        RandomAccessFile randomAccessFile=new RandomAccessFile("conf/int4.txt","rw");
         byte[] data=new byte[1024];
         int i;
         while (true){
@@ -21,10 +40,9 @@ public class HdfsService {
             if(i==-1){
                 break;
             }
-            hdfsUtil.createFile("local_int3.txt",data);
+            hdfsUtil.createFile("/input/local_int4.txt",data);
             data=new byte[1024];
         }
-
     }
 
 
