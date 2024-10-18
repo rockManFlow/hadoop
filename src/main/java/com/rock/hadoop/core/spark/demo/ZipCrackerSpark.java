949f39459ff8a6df49ac900067bd6b242093a9bf
qingyuan.cao@opay-inc.com <qingyuan.cao@opay-inc.com>
2024-10-18 17:30:58 +0800

    zip破解spark实现

---
+++ src/main/java/com/rock/hadoop/core/spark/demo/ZipCrackerSpark.java
@@ -0,0 +1,87 @@
+package com.rock.hadoop.core.spark.demo;
+
+import net.lingala.zip4j.ZipFile;
+import net.lingala.zip4j.exception.ZipException;
+import org.apache.commons.lang.StringUtils;
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+/**
+ * zip暴力破解解压
+ * 1、spark中公共参数如何处理？
+ * 2、编写的一个处理程序，会如何被处理？
+ * 3、一个spark程序是如何在集群中运行的？
+ */
+public class ZipCrackerSpark {
+    private static AtomicBoolean finish=new AtomicBoolean(false);
+    private static String unZipDirectoryPath;
+    private static final Object lock=new Object();
+
+    public static void main(String[] args) throws ZipException {
+        String zipFilePath="/Users/opayc/products/tools/src/main/java/com/kuark/tool/advance/advance20241017/method2/test02.zip";
+        String passwordFile="/Users/opayc/products/tools/src/main/java/com/kuark/tool/advance/advance20241017/method2/password_list.txt";
+        crackerZip(zipFilePath,passwordFile);
+    }
+
+    public static void crackerZip(String zipFilePath,String passFilePath) throws ZipException {
+        SparkConf sparkConf = new SparkConf().setAppName("zipCracker").setMaster("local");
+        JavaSparkContext ctx = new JavaSparkContext(sparkConf);
+        JavaRDD<String> lines = ctx.textFile(passFilePath);
+        unzip(zipFilePath,lines.toString());
+
+        ctx.stop();
+    }
+
+    public static void unzip(String zipFilePath, String passwd) throws ZipException {
+        try(ZipFile zFile = new ZipFile(zipFilePath)){// 首先创建ZipFile指向磁盘上的.zip文件
+            if (!zFile.isValidZipFile()) {   // 验证.zip文件是否合法，包括文件是否存在、是否为zip文件、是否被损坏等
+                throw new ZipException("压缩文件不合法,可能被损坏.");
+            }
+            if (zFile.isEncrypted()) {
+                zFile.setPassword(passwd.toCharArray());  // 设置密码
+            }
+            createUnZipDirectoryPath(zipFilePath);
+            zFile.extractAll(unZipDirectoryPath);      // 将文件抽出到解压目录(解压)
+            finish.compareAndSet(false,true);
+            System.out.println("正确密码："+passwd);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    private static void createUnZipDirectoryPath(String zipFilePath){
+        if(StringUtils.isEmpty(zipFilePath)){
+            System.err.println("null zipFilePath");
+            throw new RuntimeException("null zipFilePath");
+        }
+        if(!StringUtils.isEmpty(unZipDirectoryPath)){
+            return;
+        }
+
+        synchronized (lock){
+            String path=zipFilePath.substring(0,zipFilePath.lastIndexOf("/"));
+            String zipAllFileName=zipFilePath.substring(zipFilePath.lastIndexOf("/"));
+            String zipFileName=zipAllFileName.substring(0,zipAllFileName.lastIndexOf("."));
+            unZipDirectoryPath=new StringBuilder(path).append("/").append(zipFileName).append("/").toString();
+
+            // 创建File对象
+            File directory = new File(unZipDirectoryPath);
+
+            // 如果文件夹不存在，则创建文件夹
+            if (!directory.exists()) {
+                boolean result = directory.mkdirs();
+                if (result) {
+                    System.out.println("文件夹创建成功：" + unZipDirectoryPath);
+                } else {
+                    System.out.println("文件夹创建失败：" + unZipDirectoryPath);
+                }
+            }
+        }
+    }
+}
