070ab995c9161838fa4980776223b45385376b36
qingyuan.cao@opay-inc.com <qingyuan.cao@opay-inc.com>
2024-10-29 19:07:44 +0800

    mapreduce实践学习--可以正常跑

--- src/main/java/com/rock/hadoop/core/spark/demo/ZipCrackerSpark.java
+++ src/main/java/com/rock/hadoop/core/spark/demo/ZipCrackerSpark.java
@@ -6,9 +6,11 @@ import org.apache.commons.lang.StringUtils;
 import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.api.java.function.Function;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.List;
 import java.util.concurrent.atomic.AtomicBoolean;
 
 /**
@@ -23,21 +25,41 @@ public class ZipCrackerSpark {
     private static final Object lock=new Object();
 
     public static void main(String[] args) throws ZipException {
-        String zipFilePath="/Users/opayc/products/tools/src/main/java/com/kuark/tool/advance/advance20241017/method2/test02.zip";
-        String passwordFile="/Users/opayc/products/tools/src/main/java/com/kuark/tool/advance/advance20241017/method2/password_list.txt";
+        String zipFilePath="D:/myProjects/hadoop/conf/zip/test02.zip";
+        String passwordFile="D:/myProjects/hadoop/conf/zip/password_list.txt";
         crackerZip(zipFilePath,passwordFile);
     }
 
     public static void crackerZip(String zipFilePath,String passFilePath) throws ZipException {
         SparkConf sparkConf = new SparkConf().setAppName("zipCracker").setMaster("local");
         JavaSparkContext ctx = new JavaSparkContext(sparkConf);
+        //读取整个文件信息
         JavaRDD<String> lines = ctx.textFile(passFilePath);
-        unzip(zipFilePath,lines.toString());
+        //打印具体信息--这个操作是把所有集群上节点上的结果进行聚合到一个节点上，可能会占用该节点很大内存导致内存溢出，并且是串行的
+//        List<String> collect = lines.collect();
+//        for(String m:collect){
+//            unzip(zipFilePath,m);
+//        }
+
+        //这种可以并行执行，但操作需要动作才会执行
+        JavaRDD<String> map = lines.map(new Function<String, String>() {
+            @Override
+            public String call(String s) throws Exception {
+                unzip(zipFilePath,s);
+                return s;
+            }
+        });
+        map.count();//动作计数
+
 
         ctx.stop();
     }
 
     public static void unzip(String zipFilePath, String passwd) throws ZipException {
+        if(finish.get()){
+            return;
+        }
+        System.out.println("Pass:"+passwd);
         try(ZipFile zFile = new ZipFile(zipFilePath)){// 首先创建ZipFile指向磁盘上的.zip文件
             if (!zFile.isValidZipFile()) {   // 验证.zip文件是否合法，包括文件是否存在、是否为zip文件、是否被损坏等
                 throw new ZipException("压缩文件不合法,可能被损坏.");
@@ -50,7 +72,7 @@ public class ZipCrackerSpark {
             finish.compareAndSet(false,true);
             System.out.println("正确密码："+passwd);
         } catch (IOException e) {
-            e.printStackTrace();
+//            e.printStackTrace();
         }
 
     }
@@ -82,6 +104,7 @@ public class ZipCrackerSpark {
                     System.out.println("文件夹创建失败：" + unZipDirectoryPath);
                 }
             }
+            System.out.println("unZipDirectoryPath:"+unZipDirectoryPath);
         }
     }
 }
